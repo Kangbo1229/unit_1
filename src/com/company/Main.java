1f9a45c83a0d2a3f499552a9650dbd6cb2851aad
kangbo1229 <kangbo@ssfs.org>
2019-09-09 14:07:22 -0400

    practice escape characters

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,8 @@ public class Main {
 	// write your code here
         System.out.println("Hello, world!");
         System.out.println("This is another line");
+
+        System.out.println("This is \"Elly's\" computer");
+        System.out.println("I am splitting this\n over two lines");
     }
 }
