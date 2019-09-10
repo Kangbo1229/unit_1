779703465fc2e5ca3627ec34b9ccca6c23b72438
kangbo1229 <kangbo@ssfs.org>
2019-09-10 15:10:12 -0400

    Part 2 daily exercises

---
+++ src/com/company/Escape.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Escape {
+
+    public static void main(String[] args) {
+        System.out.println("What is the difference between");
+        System.out.println("a ' and a " );
+    }
+}
