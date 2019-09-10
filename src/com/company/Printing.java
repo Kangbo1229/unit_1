24499d5538468f076773e14f9a787abc65602735
kangbo1229 <kangbo@ssfs.org>
2019-09-10 15:01:19 -0400

    Completed Part 1 daily exercises

--- src/com/company/Printing.java
+++ src/com/company/Printing.java
@@ -5,9 +5,9 @@ public class Printing {
     public static void main(String[] args) {
         System.out.println("2:40");
         System.out.println("9/10/2019");
-        System.out.println("It is a great\n time to be a\n computer programmer!");
-        System.out.print("It is a great");
-        System.out.print("time to be a");
+        System.out.println("It is a great\ntime to be a\ncomputer programmer!");
+        System.out.print("It is a great ");
+        System.out.print("time to be a ");
         System.out.print("computer programmer!");
 
     }
