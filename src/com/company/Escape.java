a5bf33916e236aa97a5e3514fec17c93ac8261dd
kangbo1229 <kangbo@ssfs.org>
2019-09-12 08:56:42 -0400

    Completed Part 2 daily exercises

--- src/com/company/Escape.java
+++ src/com/company/Escape.java
@@ -4,6 +4,20 @@ public class Escape {
 
     public static void main(String[] args) {
         System.out.println("What is the difference between");
-        System.out.println("a ' and a " );
+        System.out.println("a ' and a \"?  Or between a \" and a \\\"?\n");
+        System.out.println("One is what we see when we're typing our program.");
+        System.out.println("The other is what appears on the \"console.\"");
+        System.out.print("  \\/\n" +
+                " \\\\//\n" +
+                "\\\\\\///\n" +
+                "///\\\\\\\n" +
+                " //\\\\\n" +
+                "  /\\\n");
+        System.out.print("  \\/\n");
+        System.out.print(" \\\\//\n");
+        System.out.print("\\\\\\///\n");
+        System.out.print("///\\\\\\\n");
+        System.out.print(" //\\\\\n");
+        System.out.print("  /\\\n");
     }
 }
