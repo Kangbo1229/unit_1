277e64ade474299ebcfa457c5daa0af141660a72
kangbo1229 <kangbo@ssfs.org>
2019-09-13 09:23:29 -0400

    Completed Q7 1.1 daily exercises

--- src/com/company/Meta.java
+++ src/com/company/Meta.java
@@ -4,8 +4,11 @@ public class Meta {
 
     public static void main(String[] args) {
 
-        System.out.print("public class Hello {");
-        System.out.print("public static void main(String[] args) {");
+        System.out.println("public class Hello {");
+        System.out.println("   public static void main(String[] args) {");
+        System.out.println("      System.out.println(\"Hello, world!\");");
+        System.out.println("   }");
+        System.out.println("}");
 
 
     }
