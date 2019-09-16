77a33cbe3db0f3ce42c6e8a635fd875c5ccab94b
kangbo1229 <kangbo@ssfs.org>
2019-09-16 11:10:55 -0400

    Completed TotalWithTax

---
+++ src/com/company/TotalWithTax.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class TotalWithTax {
+    public static void main(String[] args) {
+        final double TAXRATE=0.0825;
+        double price=52.75;
+        double tax=price*TAXRATE;
+        double totalcost=price +tax;
+        System.out.println("Price: $"+price);
+        System.out.println("Sales Tax: $"+tax);
+        System.out.println("Total: $"+totalcost);
+    }
+}
