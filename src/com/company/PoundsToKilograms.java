6974bccb859227894c231ccd002304746578e362
kangbo1229 <kangbo@ssfs.org>
2019-09-16 11:02:08 -0400

    Completed PoundsToKilograms

---
+++ src/com/company/PoundsToKilograms.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class PoundsToKilograms {
+    public static void main(String[] args) {
+        double ibs=115.0;
+        double kg=ibs/2.205;
+        System.out.print(ibs+" pounds = "+kg+" kilograms");
+
+
+
+    }
+}
