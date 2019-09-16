2be6e0fc47101d6cd99976b41591764c71ec8323
kangbo1229 <kangbo@ssfs.org>
2019-09-16 10:52:44 -0400

    Completed RectangleArea

--- src/com/company/Painting.java
+++ src/com/company/Painting.java
@@ -3,7 +3,7 @@ package com.company;
 public class Painting {
 
     public static void main(String[] args) {
-        final int area_per_gallon=350;
+        final int AREA_PER_GALLON=350;
         int length=40;
         int width=32;
         int height=16;
@@ -12,7 +12,7 @@ public class Painting {
         int number_of_doors=2;
         int number_of_windows=4;
         double total_surface_area= length*height*2+width*height*2+length*width - area_of_door*number_of_doors - area_of_window*number_of_windows;
-        double total_gallon=total_surface_area/area_per_gallon;
+        double total_gallon=total_surface_area/AREA_PER_GALLON;
         System.out.println("You will need "+total_gallon+" gallons of paint");
 
     }
