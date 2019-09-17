a8b4c9ab87ba4edf31c898c1b7525cbb439bdd85
kangbo1229 <kangbo@ssfs.org>
2019-09-17 12:15:14 -0400

    Completed TempConversion

--- src/com/company/TempConversion.java
+++ src/com/company/TempConversion.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class TempConversion {
     public static void main(String[] args) {
-        double Fahrenheit=80;
+        double Fahrenheit=80.0;
         double Celsius=(Fahrenheit-32)*5/9;
         System.out.println(Fahrenheit+"°F = "+Celsius+"°C");
     }
