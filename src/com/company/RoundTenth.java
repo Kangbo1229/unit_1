0405480c0a53c7989b532094e63806cafc6e147f
kangbo1229 <kangbo@ssfs.org>
2019-09-19 15:44:41 -0400

    Completed RoundTenth

--- src/com/company/RoundTenth.java
+++ src/com/company/RoundTenth.java
@@ -6,6 +6,12 @@ public class RoundTenth {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+        System.out.print("Decimal number?");
+        double value1=scan.nextDouble();
+        double value2=(double)(value1*10);
+        double value3=(int)(value2+0.5);
+        double value4=value3/10;
+        System.out.print(value1+" round to the nearest tenth is "+value4);
 
     }
 }
